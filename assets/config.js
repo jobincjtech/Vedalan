4dc330127ee5d2d3c0f98b19764ff062fb780164
jobincjtech <jobin.cj.vmd@gmail.com>
2026-04-07 20:38:02 +0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.FIRE_APP_CONFIG = {
   SITE_NAME: "AHM Fire Extinguisher Inspection",
-  APPS_SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxhL_DvODEpuel0nLAnRTACgbDgj0Qfdce0kFBo-jw1PR8H4pvsjniBa_YReABQByiu1A/exec",
   SESSION_HOURS: 8,
   BRAND_CODE: "AHM/HSE/FEX"
 };
